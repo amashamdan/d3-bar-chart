c639a6df12ed28bef7dc818338004078644a68f2
Amer Hamdan <amashamdan@gmail.com>
2016-08-25 13:31:13 -0700

    Feat: Chart is now displayed with no formatting.
    AJAX call is placed to fetch the data to be visualized.
    Data is manipulated to make it ready for visualization.
    plot function creates the svg and plot the data with no formatting.
    Next, padding will be added to allow space for other components.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,58 @@
-//Call for data json
-console.log("HH")
+var url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+var gdpData = [];
+var modifiedData = [];
+$.ajax({
+	url: url,
+	dataType: "json",
+	success: function(data) {
+		for (var point in data.data) {	
+			gdpData.push(data.data[point]);
+		};
+		console.log(gdpData[0]);
+		for (var point in gdpData) {
+			var fullDate = gdpData[point][0];
+			fullDate = fullDate.split("-");
+			modifiedData.push([fullDate[0], gdpData[point][1]]);
+		}
+		plot();
+	},
+	error: function () {
+		alert("Data couldn't be retreived.");
+	}
+});	
+
+// should be a function called from ajax, if not in function. it'll execute before the response comes back and nothing will be displayed.
+function plot() {
+	var width = 900;
+	var height = 500;
+	var padding = 0;
+	var chart = d3.select("body").append("svg").attr("height", height).attr("width", width);
+	chart.style("background-color", "white");
+
+	var xScale = d3.scale.linear()
+						.domain([0, d3.max(modifiedData, function(d) {return d[0];})])
+						.range([padding, width - padding]);
+
+	var yScale = d3.scale.linear()
+						.domain([0, d3.max(modifiedData, function(d) {return d[1];})])
+						.range([padding, height - padding]);
+
+	chart.selectAll("rect")
+		.data(modifiedData)
+		.enter()
+		.append("rect")
+		.attr("x", function(d, i) {
+			return (width / modifiedData.length) * i;
+		})
+		.attr("y", function(d) {
+			return height - yScale(d[1]);
+		})
+		.attr("width", function(d) {
+			// +1 here makes the bars overlap and hide the borders between them
+			return width / modifiedData.length + 1;
+		})
+		.attr("height", function(d) {
+			return yScale(d[1]);
+		})
+		.attr("fill", "blue");
+}
