1a3c25b1c31801baf6c263d3de9f7a1216bad96d
Amer Hamdan <amashamdan@gmail.com>
2016-08-25 15:44:40 -0700

    Feat: Added title, axes and their labels to the chart.
    Axes, their labels and a title are added to the chart.
    labels and title are added by appending text to the chart.

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $.ajax({
 
 // should be a function called from ajax, if not in function. it'll execute before the response comes back and nothing will be displayed.
 function plot() {
-	var padding = 50;
+	var padding = 100;
 	var width = 1000;
 	var height = 600;
 	var chart = d3.select("body").append("svg").attr("height", height).attr("width", width);
@@ -31,7 +31,7 @@ function plot() {
 
 	var xScale = d3.scale.linear()
 						.domain([0, d3.max(modifiedData, function(d) {return d[0];})])
-						.range([padding, width - padding]);
+						.range([padding, width - padding / 2]);
 
 	var yScale = d3.scale.linear()
 						.domain([0, d3.max(modifiedData, function(d) {return d[1];})])
@@ -45,7 +45,8 @@ function plot() {
 		.enter()
 		.append("rect")
 		.attr("x", function(d, i) {
-			return ((width - 2*padding)/ modifiedData.length) * i + padding;
+			// we have full padding from left and half padding from right.. totalis 1.5.
+			return ((width - 1.5*padding)/ modifiedData.length) * i + padding;
 		})
 		.attr("y", function(d) {
 			return height - yScale(d[1]);
@@ -61,4 +62,57 @@ function plot() {
 			// Math.floor in needed, rgb only accepts integers.
 			return "rgb(0,0," + Math.floor(colorScale(d[1])) + ")";
 		});
+	var xAxis = d3.svg.axis()
+				.scale(xScale)
+				.orient("bottom");
+	chart.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0, "+ (height - padding) +" )")
+		.style("stroke-width", "3px")
+		.call(xAxis);
+	var yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient("left");
+	chart.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate("+padding+", 0)")
+		.call(yAxis);
+
+	chart.append("text")
+		.attr("x", width / 2)
+		.attr("y", padding / 2)
+		.text("Gross Domestic Product in the U.S (1947 - 2015)")
+		.attr("text-anchor", "middle")
+		.attr("font-family", "arial")
+		.attr("fill", "gray")
+		.attr("font-size", "1.5em");
+
+	chart.append("text")
+		.attr("x", width / 2)
+		.attr("y", height - padding / 2)
+		.text("Year")
+		.attr("text-anchor", "middle")
+		.attr("font-family", "arial")
+		.attr("fill", "gray")
+		.attr("font-size", "1em");
+
+	chart.append("text")
+		.attr("x", width / 2)
+		.attr("y", height - padding / 4)
+		.text("Source: Federal Reserve Economic Data")
+		.attr("text-anchor", "middle")
+		.attr("font-family", "arial")
+		.attr("fill", "gray")
+		.attr("font-size", "0.8em");
+
+	// since text is rotated, x becomes y and vice versa (it seems that center of rotation is point (0,0))
+	chart.append("text")
+		.attr("x", -height / 2)
+		.attr("y", padding / 3)
+		.text("Gross Demostic Product ($Billion)")
+		.attr("text-anchor", "middle")
+		.attr("font-family", "arial")
+		.attr("fill", "gray")
+		.attr("font-size", "1em")
+		.attr("transform", "rotate(-90)");
 }
